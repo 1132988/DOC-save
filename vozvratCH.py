04e198a0ca385c75a0091e95e40576f83b0049ea
Dmitrii <ddeal087@yandex.ru>
2023-12-22 18:00:52 +0300

    modified:   vozvratCH.py

--- vozvratCH.py
+++ vozvratCH.py
@@ -1,5 +1,6 @@
 #Возврат для частных лиц
 import os
+import re
 import sys
 import sqlite3
 import datetime
@@ -32,10 +33,21 @@ def folders(): #Проверка и создание папки
     else:
         print("Папка уже существует")
 def sn_server(): # Находим индекс "SSF" и ещё 6 цифр серийного номера сервера или рабочей станции из примечания (код этой функции задействован, сама функция - нет)
-    index = note.find("SSF")  
-    snserv_dir = note[index:index+9]  
+    index = note.find("SSF")
+    snserv_dir = note[index:index+9]
     print(snserv_dir)
 def check_file(): #Проверка на наличие файла
+    test = f'D:/Documents/{data_y}/{data_f}/{snsrv}/{act}возвратЧЛ.docx'
+    if os.path.exists(test) and os.path.isfile(test):
+        print(f"Есть файл с таким названием {act}возвратЧЛ.docx")
+        print('Размер:', os.path.getsize(test) // 1024, 'Кб')
+        print('Перезаписать файл нельзя, отредактируйте квитанцию', f'{act}возвратЧЛ.docx вручную!')
+        sys.exit(1)
+        input("Нажмите Enter для закрытия программы")
+    else:
+        doc.save(f'D:/Documents/{data_y}/{data_f}/{snsrv}/{act}возвратЧЛ.docx')  # Место куда сохраняется этот файл
+        print("Файл сохранен")
+def database_and_filecheck(): #Создание базы данных ВОЗМОЖНО МОГУТ БЫТЬ ПРОБЛЕМЫ.
     test = f'D:/Documents/{data_y}/{data_f}/{snsrv}/{act}возвратЧЛ.docx'
     if os.path.exists(test) and os.path.isfile(test):
         print(f"Есть файл с таким названием {act}возвратЧЛ.docx")
@@ -44,27 +56,38 @@ def check_file(): #Проверка на наличие файла
         #sys.exit(1)
         input("Нажмите Enter для закрытия программы")
     else:
+        conn = sqlite3.connect('trial_guarantee.db')
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS vozvrat_ch (id INTEGER PRIMARY KEY, act INTEGER, sn TEXT, snsrv TEXT, note TEXT, act_p INTEGER)''')
+        cursor.execute("INSERT INTO vozvrat_ch (act, snsrv, note, sn) VALUES (?, ?, ?, ?)", (act, snsrv, note, sn)) #,act_p))
+        conn.commit()
+        conn.close()
+        
+        conn = sqlite3.connect('trial_guarantee.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT priyom_ch.act FROM priyom_ch JOIN vozvrat_ch ON priyom_ch.snsrv = vozvrat_ch.snsrv") #Думать над этим!!!
+        act_p = cursor.fetchall()
+        act_p = re.sub("[(|,|)]","", str(act_p[1]))
+        print(act_p)
+        cursor.execute("INSERT INTO vozvrat_ch (act_p)", (act_p))
+        conn.commit()
+        conn.close()           
+        
+        context = {'act': act, 'model': model, 'sn': sn, 'note': note, 'act_p': act_p, 'date': data} #Подумать.
+        doc.render(context)
         doc.save(f'D:/Documents/{data_y}/{data_f}/{snsrv}/{act}возвратЧЛ.docx')  # Место куда сохраняется этот файл
         print("Файл сохранен")
-def database(): #Создание базы данных ВОЗМОЖНО МОГУТ БЫТЬ ПРОБЛЕМЫ.
-    conn = sqlite3.connect('trial_guarantee.db')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS vozvrat_ch (id INTEGER PRIMARY KEY, act INTEGER, sn TEXT, snsrv TEXT, note TEXT, act_p INTEGER)''')
-    cursor.execute("INSERT INTO vozvrat_ch (act, snsrv, note, sn) VALUES (?, ?, ?, ?)", (act, snsrv, note, sn)) #,act_p))
-    #cursor.execute("SELECT (act) FROM priyom_ch WHERE priyom_ch (snsrv) = vozvrat_ch (snsrv)", (act_p)) #Думать над этим!!!
-    conn.commit()
-    conn.close()
-
+    
 doc = DocxTemplate(r'C:\Program Files\Python38\pythonDOCX\Акт_возвратаЧЛ.docx')
 print("Акт возврата для частных лиц")
 act = input('Акт №: ')
 model = input('модель: ')
 sn = input('Serial Number оборудования: ')
 note = input('Примечание, что было сделано и тп (Обязательно ввести SN Сервера или рабочей станции, далее по желанию): ')
-act_p = input('Ранее принято по акту приёма: ') #Думать над этим
+#act_p = input('Ранее принято по акту приёма: ') #Думать над этим
 
 index = note.find("SSF")  # Находим индекс начала "SSF"
-snserv_dir = note[index:index+9]  
+snserv_dir = note[index:index+9]
 print(snserv_dir)  # Выводим результат
 snsrv = snserv_dir
 
@@ -76,10 +99,10 @@ data = data_object.strftime('%d %B %Y')
 data_f = data_object.strftime('%m %Y')
 data_y = data_object.strftime('%Y')
 
-context = {'act': act, 'model': model, 'sn': sn, 'note': note, 'act_p': act_p, 'date': data} #Подумать.
-doc.render(context)
+#context = {'act': act, 'model': model, 'sn': sn, 'note': note, 'act_p': act_p, 'date': data} #Подумать.
+#doc.render(context)
 folders()
-database()
-check_file()
+#database()
+#check_file()
+database_and_filecheck()
 input("Нажмите Enter для закрытия программы")
-
