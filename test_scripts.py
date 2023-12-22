2517ef2782e0afbfa22b356945775740cc99a09e
Dmitrii <ddeal087@yandex.ru>
2023-12-22 12:50:42 +0300

    modified:   test_scripts.py

--- test_scripts.py
+++ test_scripts.py
@@ -1,4 +1,11 @@
-import os
+#Сбор акта из другой таблицы БД, и его обработка.
 
-new_dir = input('путь_к_новой_директории: ')
-os.environ['PWD'] = new_dir
+
+import sqlite3
+import re
+conn = sqlite3.connect('trial_guarantee.db')
+cursor = conn.cursor()
+cursor.execute("SELECT priyom_ch.act FROM priyom_ch JOIN vozvrat_ch ON priyom_ch.snsrv = vozvrat_ch.snsrv") #Думать над этим!!!
+act_p = cursor.fetchall()
+act_p = re.sub("[(|,|)]","", str(act_p[1])) #Избавляемся от повторения, а также от других знаков
+print(act_p)
